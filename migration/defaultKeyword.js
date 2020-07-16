a113c30d54280f08716859e98f4c19f2f091a956
truongginjs <truongginjs@gmail.com>
2020-07-16 20:27:20 +0700

    limit skip

--- migration/defaultKeyword.js
+++ migration/defaultKeyword.js
@@ -13,10 +13,11 @@ const run = async () => {
     }
     const url = "https://vi.wikipedia.org/wiki/Th%E1%BB%83_lo%E1%BA%A1i:Ph%C6%B0%E1%BB%9Dng_thu%E1%BB%99c_Qu%E1%BA%ADn_9"
 
-    const $ = await fetchHtmlFromUrl(url);
+    const $ = await fetchHtmlFromUrl(url);//cai nay de lay het html thoi
 
     //RegExp("\b(qu[ậa]n 9)\b",iu)
     //quận 9
+    //cai đống này là để lấy regex
     data.regex += '\\b('
     $('div.mw-content-ltr>div.mw-category li a').each((i, e) => {
         const text = $(e).text().split(/\s*(?:[;.,\(\)]|$)\s*/)[0].toLowerCase()
@@ -26,6 +27,7 @@ const run = async () => {
         data.keywords.push(text)
     })
     data.regex = data.regex.substr(0,data.regex.length-1)+')\\b'
+
     // console.log(data.regex);
     Place.create(data).then(res => console.log(res)).catch(err => console.log(err))
 }
